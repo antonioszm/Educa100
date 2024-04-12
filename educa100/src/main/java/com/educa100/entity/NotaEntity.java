5caafadd2b9c231d9080999012122bbf663652bf
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-04-12 20:56:58 -0300

    feat: entidade nota

---
+++ educa100/src/main/java/com/educa100/entity/NotaEntity.java
@@ -0,0 +1,31 @@
+package com.educa100.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "nota")
+public class NotaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "id_aluno", nullable = false)
+    private int id_aluno;
+
+    @OneToOne
+    @JoinColumn(name = "id_professor", nullable = false)
+    private int id_professor;
+
+    @OneToOne
+    @JoinColumn(name = "id_materia", nullable = false)
+    private int id_materia;
+
+    private double valor;
+
+    private Date data;
+}
