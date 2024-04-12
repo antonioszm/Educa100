fad9c5c6e7c634c30b37da0a31bcf9b1386110ca
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-04-12 20:45:10 -0300

    feat: entidade materia

---
+++ educa100/src/main/java/com/educa100/entity/MateriaEntity.java
@@ -0,0 +1,20 @@
+package com.educa100.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "materia")
+public class MateriaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String nome;
+
+    @OneToMany
+    @JoinColumn(name = "id_curso", nullable = false)
+    private int id_curso;
+}
