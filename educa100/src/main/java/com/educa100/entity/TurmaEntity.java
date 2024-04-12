afc728eee0678b667b560e904ad871db79451445
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-04-12 20:11:04 -0300

    feat: entidade turma

---
+++ educa100/src/main/java/com/educa100/entity/TurmaEntity.java
@@ -0,0 +1,25 @@
+package com.educa100.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "turma")
+public class TurmaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String nome;
+
+    @ManyToOne
+    private List<AlunosEntity> alunos;
+
+    @OneToMany
+    @JoinColumn(name = "id_curso", nullable = false)
+    private int id_curso;
+}
