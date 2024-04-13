102be12c5c2e217eba2d0ae5ec6b365b5c1c4160
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-04-13 19:45:12 -0300

    feat: criação Service e ServiceImpl de Aluno

---
+++ educa100/src/main/java/com/educa100/service/AlunoService.java
@@ -0,0 +1,13 @@
+package com.educa100.service;
+
+import com.educa100.datasource.entity.AlunoEntity;
+
+import java.util.List;
+
+public interface AlunoService {
+    public AlunoEntity salvar(AlunoEntity aluno);
+    public void atualizar(AlunoEntity aluno);
+    public void removerPorId(int id);
+    public List<AlunoEntity> listarTodos();
+    public AlunoEntity listarPorId(int id);
+}
