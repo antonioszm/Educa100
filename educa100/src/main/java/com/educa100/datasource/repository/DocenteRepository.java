ad3b74b202b105fe2cadfe899c9f5ade1ad22dff
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-04-13 12:20:07 -0300

    feat: criação Docente repository

---
+++ educa100/src/main/java/com/educa100/datasource/repository/DocenteRepository.java
@@ -0,0 +1,23 @@
+package com.educa100.datasource.repository;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+
+@Repository
+public interface DocenteRepository extends JpaRepository {
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Docente SET nome = :nome, data_entrada = :data_entrada, id_usuario = :id_usuario WHERE id = :id", nativeQuery = true)
+    void update(@Param("id") int id,
+                @Param("nome") String nome,
+                @Param("data_entrada")Date data_entrada,
+                @Param("id_usuario") int id_usuario
+    );
+}
