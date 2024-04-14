b5e963021781d8847700f651ec9d80a3826148b8
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-04-13 22:04:30 -0300

    feat: listar notas por id aluno

--- educa100/src/main/java/com/educa100/service/NotaService.java
+++ educa100/src/main/java/com/educa100/service/NotaService.java
@@ -1,6 +1,7 @@
 package com.educa100.service;
 
 import com.educa100.datasource.entity.NotaEntity;
+import com.educa100.datasource.entity.TurmaEntity;
 
 import java.util.List;
 
@@ -8,6 +9,7 @@ public interface NotaService {
     public NotaEntity salvar(NotaEntity nota);
     public void atualizar(int id);
     public void removerPorId(int id);
+    public List<NotaEntity> listarPorIdAluno(int id);
     public List<NotaEntity> listarTodos();
     public NotaEntity listarPorId(int id);
 }
