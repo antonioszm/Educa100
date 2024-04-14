8347323d428be704e1e18fc449136671ff4c1e64
antonioszm <75432982+antonioszm@users.noreply.github.com>
2024-04-14 20:27:21 -0300

    feat: DTO's para turma (request e response)

---
+++ educa100/src/main/java/com/educa100/controller/dto/request/TurmaRequest.java
@@ -0,0 +1,11 @@
+package com.educa100.controller.dto.request;
+
+import com.educa100.datasource.entity.AlunoEntity;
+import com.educa100.datasource.entity.DocenteEntity;
+import com.educa100.datasource.entity.MateriaEntity;
+import com.educa100.datasource.entity.TurmaEntity;
+
+import java.util.List;
+
+public record TurmaRequest(String nome, List<AlunoEntity> alunos, DocenteEntity professor, int id_curso) {
+}
